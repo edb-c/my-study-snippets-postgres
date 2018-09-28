80ab2117331e7be6efc4ca502b4c66d997d47f0c
edb-c <edb.cofresi@gmail.com>
2018-09-28 12:30:11 -0400

    modified database adapter

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20180915005754) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "snippets", force: :cascade do |t|
     t.string   "snippet_category"
     t.string   "snippet_name"
