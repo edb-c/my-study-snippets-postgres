ff0948295ad2267822730dd2b431a09d60c7b423
edb-c <edb.cofresi@gmail.com>
2018-09-28 15:46:45 -0400

    testing postgres

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,6 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.connection.tables.each do |t|
-  ActiveRecord::Base.connection.reset_pk_sequence!(t)
-end
 #ActiveRecord::Base.establish_connection(
 #  :adapter => "sqlite3",
 #  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
